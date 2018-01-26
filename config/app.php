a11e85d896fd2482555d2548f08817a9e1ee0b26
Giacomo Giovanni Delfini <gdelfini@de.caecorp.cae.com>
2018-01-26 17:27:28 +0100

    added APIs

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*Barryvdh cors*/
+        Barryvdh\Cors\ServiceProvider::class,  
+
     ],
 
     /*
