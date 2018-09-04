4514e019de93a0e37fc366b77500c4c4e0e4fb24
Giacomo Giovanni Delfini <gdelfini@de.caecorp.cae.com>
2018-09-04 09:57:34 +0200

    minor changes

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class,50)->create();
+        factory(App\User::class,50)->create(); //con make si creerebbe un array sul quale ciclare senza metterlo ne db
+        
     }
 }
